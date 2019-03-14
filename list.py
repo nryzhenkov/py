432939065a54bf61cb813fa252546d38773ecadc
nryzhenkov <nryzhenkov@yandex.ru>
2019-03-14 21:32:58 +0300

    add hexdec_list
    fix errors

--- list.py
+++ list.py
@@ -1,10 +1,11 @@
 decimal_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+hexadecimal_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 
 empty_list_0 = []
 empty_list_1 = list()
 
 empty_list_2D = [[]]
 
-copy_of_numbers = list(numbers)
+copy_of_numbers = list(decimal_digits)
 
-print(numbers[-3])
+print(hexadecimal_digits[-3])
