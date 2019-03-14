96cd495e98f30073d546ce30d2661ef678583d83
nryzhenkov <nryzhenkov@yandex.ru>
2019-03-14 21:28:18 +0300

    add more lists

--- list.py
+++ list.py
@@ -1,1 +1,10 @@
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+decimal_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+empty_list_0 = []
+empty_list_1 = list()
+
+empty_list_2D = [[]]
+
+copy_of_numbers = list(numbers)
+
+print(numbers[-3])
